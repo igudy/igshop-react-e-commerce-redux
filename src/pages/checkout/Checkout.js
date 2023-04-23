3f3a1c07d2c49624138ecf638f269fe100f539e2
goodness.igunma <goodness.igunma@interswitchgroup.com>
2023-04-23 03:04:13 +0100

    Config modified for prod.

--- src/pages/checkout/Checkout.js
+++ src/pages/checkout/Checkout.js
@@ -40,7 +40,7 @@ const Checkout = () => {
   useEffect(() => {
     // http://localhost:4242/create-payment-intent
     // Create PaymentIntent as soon as the page loads
-    fetch("http://localhost:4242/create-payment-intent", {
+    fetch("http://igshop-react-e-commerce.herokuapp.com/create-payment-intent", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
